440a9ce1a2ef48044ead5b0d102a23f6654b91c5
mu-126 <mp1152005@gmail.com>
2025-10-17 20:35:49 +0900

    課題1テスト

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>test1</p>
+        <p>課題1</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
