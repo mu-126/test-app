3a70d2c0349547ef47373bddbd9c32bf108c2aa8
mu-126 <mp1152005@gmail.com>
2026-01-05 06:15:18 +0900

    課題2：実装

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PostList from "./PostList";
+import PostDetail from "./PostDetail";
 
 const App = ({ src }) => {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <dl className="max-w-3xl mx-auto my-16 space-y-10">
-        {src.map((elem) => (
-          <>
-            <div key={elem.id} className="border border-gray-300 p-4  shadow-sm text-sm">
-              <dt>
-                <div className="flex justify-between text-sm  text-gray-500 mb-1">
-                  <span>{new Date(elem.createdAt).toLocaleDateString("ja-JP", { year: "numeric", month: "numeric", day: "numeric" })}</span>
-                  <div className="flex gap-2 flex-wrap pr-32">
-                    {elem.categories.map((cat) => (
-                      <span key={cat} className="bg-white   text-blue-700 border border-blue-700 px-2 py-0.5  rounded text-sm">
-                        {cat}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-                <h1 className="text-2xl pb-6 text-gray-700">APIで取得した{elem.title}</h1>
-              </dt>
 
-              {/* HTMLとして描画 */}
-              <dd className="text-gray-700 line-clamp-2" dangerouslySetInnerHTML={{ __html: elem.content }} />
-            </div>
-          </>
-        ))}
-      </dl>
-    </div>
+      <Routes>
+        <Route path="/" element={<PostList src={src} />} />
+        <Route path="/posts/:id" element={<PostDetail src={src} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/PostList.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+const PostList = ({ src }) => {
+  return (
+    <div>
+      <dl className="max-w-3xl mx-auto my-16 space-y-10">
+        {src.map((elem) => (
+          <>
+            <Link key={elem.id} to={`/posts/${elem.id}`} className="block border border-gray-300 p-4  shadow-sm text-sm">
+              <dt>
+                <div className="flex justify-between text-sm  text-gray-500 mb-1">
+                  <span>{new Date(elem.createdAt).toLocaleDateString("ja-JP", { year: "numeric", month: "numeric", day: "numeric" })}</span>
+                  <div className="flex gap-2 flex-wrap pr-32">
+                    {elem.categories.map((cat) => (
+                      <span key={cat} className="bg-white   text-blue-700 border border-blue-700 px-2 py-0.5  rounded text-sm">
+                        {cat}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <h1 className="text-2xl pb-6 text-gray-700">APIで取得した{elem.title}</h1>
+              </dt>
+
+              {/* HTMLとして描画 */}
+              <dd className="text-gray-700 line-clamp-2" dangerouslySetInnerHTML={{ __html: elem.content }} />
+            </Link>
+          </>
+        ))}
+      </dl>
+    </div>
+  );
+};
+
+export default PostList;
