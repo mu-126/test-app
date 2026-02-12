9ca3a33e1ebbc4dab78c1f54d2d1cbf7f0da5c69
mu-126 <mp1152005@gmail.com>
2026-02-12 19:56:37 +0900

    課題4：実装

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PostList from "./PostList";
 import PostDetail from "./PostDetail";
 import NotFoundPage from "./NotFoundPage";
+import ContactForm from "./ContactForm";
 
 const App = ({ src }) => {
   return (
@@ -13,6 +14,7 @@ const App = ({ src }) => {
       <Routes>
         <Route path="/" element={<PostList src={src} />} />
         <Route path="/posts/:id" element={<PostDetail src={src} />} />
+        <Route path="/ContactForm" element={<ContactForm />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
